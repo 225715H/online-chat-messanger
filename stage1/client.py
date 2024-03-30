faac1f7d35a82bca4c2b7d719186cad53cae06d3
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-03-30 20:09:20 +0900

    add function of disconnected on stage1

--- stage1/client.py
+++ stage1/client.py
@@ -1,27 +1,61 @@
 import socket
 import threading
+from datetime import datetime, timedelta
+import time
+
+def remove_inactive_clients():
+    while True:
+        current_time = datetime.now()
+        if (current_time - last_time) > timedelta(minutes=1):
+            print('No activity for 5 minutes. You have been disconnected.')
+            sock.close()
+        # 30秒ごとにチェック
+        time.sleep(10)
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = '0.0.0.0'
 server_port = 9001
 
-username = input('Enter your username: ').encode()
+last_time = datetime.now()
+
+username = input('Enter your username: ')
 print(username, server_address, server_port)
-sock.sendto(username, (server_address, server_port))
+sock.sendto(username.encode(), (server_address, server_port))
 can_connect, _ = sock.recvfrom(4096)
 
 def receive():
+    global flag
+    try:
+        while flag:
+            data, _ = sock.recvfrom(4096)
+            data = data.decode()
+            print('\n{}\nEnter your message.'.format(data))
+    except OSError as e:
+        if e.errno == 9:
+            print('You have been disconnected.')
+        else:
+            False
+
+flag = True
+
+try:
     while True:
-        data, _ = sock.recvfrom(4096)
-        user, _ = sock.recvfrom(4096)
-        print('\nreceived {} {}\nEnter your message.'.format(data, user))
+        responce = threading.Thread(target=receive)
+        responce.start()
+        threading.Thread(target=remove_inactive_clients).start()
+        
+        message = input()
+        last_time = datetime.now()
+        message = username + ': ' + message
+        print('sending {!r}'.format(message))
+        sent = sock.sendto(message.encode(), (server_address, server_port))
 
-while True:
-    response = threading.Thread(target=receive)
-    response.start()
-    message = input('').encode()
+except KeyboardInterrupt:
+    print('You have been disconnected.')
 
-    print('sending {!r}'.format(message))
-    sent = sock.sendto(message, (server_address, server_port))
-
+finally:
+    flag = False
+    print('closing socket')
+    sock.close()
--- stage1/server.py
+++ stage1/server.py
@@ -1,17 +1,29 @@
 import socket
 import threading
+from datetime import datetime, timedelta
+import time
 
+# クライアントを削除するタイマーを開始
+def remove_inactive_clients():
+    while True:
+        current_time = datetime.now()
+        for client_address, last_time in last_message_time.items():
+            if (current_time - last_time) > timedelta(minutes=1):
+                remove_client(client_address)
+        # 30秒ごとにチェック
+        time.sleep(10)
+
+def remove_client(client_address):
+    user = clients.pop(client_address)
+    print(f'{user} disconnected')
 
 def broadcast(message):
     for c, user in clients.items():
         try:
             sock.sendto(message, c)
-            sock.sendto(user, c)
-            print(message, 'send to', user)
-        except Exception as e:
-            print(e)
-            print('Error broadcasting to', c)
-            continue
+            print(f'{user}: {message}')
+        except:
+            remove_client(c)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = '0.0.0.0'
@@ -19,11 +31,15 @@ server_port = 9001
 print('starting up on {} port {}'.format(server_address, server_port))
 sock.bind((server_address, server_port))
 clients = {}
+last_message_time = {}
 print('Server is running...')
+threading.Thread(target=remove_inactive_clients).start()
 
 while True:
     data, client_address = sock.recvfrom(4096)
+    last_message_time[client_address] = datetime.now()
     user = data.decode()
+    user = user[:user.find(':') + 1] if ':' in user else user
     if client_address not in clients:
         clients[client_address] = user
         print(client_address[1], 'connected as', user)
